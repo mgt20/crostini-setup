0299f1a8def8031fe1dab01dfcdd5d4c91f72e9f
mgt20 <66187533+mgt20@users.noreply.github.com>
2021-04-07 11:10:39 -0700

    fixing typo

--- docker-crostini-setup.sh
+++ docker-crostini-setup.sh
@@ -43,7 +43,7 @@ get_docker() {
     sudo curl -fsSL "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     sudo curl -fsSL "https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose" -o /etc/bash_completion.d/docker-compose
-    newgro docker
+    newgrp docker
 
 }
 
