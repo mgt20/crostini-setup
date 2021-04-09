499106b43185921e0717640686c1d91aa8465163
mgt20 <66187533+mgt20@users.noreply.github.com>
2021-04-09 02:24:00 -0700

    Update docker-crostini-setup.sh

--- docker-crostini-setup.sh
+++ docker-crostini-setup.sh
@@ -33,17 +33,18 @@ get_docker() {
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt-get update
-	sudo apt-get install docker-ce -y
-    sudo apt-get install docker-ce-cli -y
-    sudo apt-get install containerd.io -y
-    sudo groupadd docker
+	sudo apt-get install \
+		docker-ce \
+		docker-ce-cli \
+		containerd.io -y
+    	sudo groupadd docker
 	me=`whoami`
 	sudo usermod -aG docker ${me}
-    COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
-    sudo curl -fsSL "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    sudo curl -fsSL "https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose" -o /etc/bash_completion.d/docker-compose
-    newgrp docker
+    	COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+    	sudo curl -fsSL "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    	sudo chmod +x /usr/local/bin/docker-compose
+    	sudo curl -fsSL "https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose" -o /etc/bash_completion.d/docker-compose
+    	newgrp docker
 
 }
 
